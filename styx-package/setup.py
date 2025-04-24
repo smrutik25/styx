7e6a4e63f396a1615740eb4e3d765fa120c57e8f
kPsarakis <kpsarakis94@gmail.com>
2025-04-24 17:16:30 +0200

    add compression and various fixes to the stop and restart (works correctly after this commit)

--- styx-package/setup.py
+++ styx-package/setup.py
@@ -15,7 +15,10 @@ setuptools.setup(
         # async logging
         'aiologger>=0.7.0,<1.0',
         # hashing strings
-        'cityhash>=0.4.7,<1.0.0',
+        'cityhash>=0.4.8,<1.0.0',
+        # compression
+        'zstandard>=0.23.0,<1.0.0'
+        # schema validation
         'pydantic>=2.10',
     ],
     python_requires='>=3.13',
