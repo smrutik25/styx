6be26662a257d2afb325b357511613d42224aff5
kPsarakis <kpsarakis94@gmail.com>
2025-05-10 22:43:43 +0200

    init the squall approach with the reactive part

--- styx-package/styx/common/message_types.py
+++ styx-package/styx/common/message_types.py
@@ -38,5 +38,8 @@ class MessageType(IntEnum):
     SnapTakeSnapshot = 32
     SnapNAssigned = 33
     ResponseToRoot = 34
+    ReceiveMigrationHashes = 35
+    RequestRemoteKey = 36
+    ReceiveRemoteKey = 37
     AlignStart = 100
     AlignEnd = 101
