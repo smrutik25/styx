eb48ff6d7d57aea0ff4bfcc1a86a18eba40fe1a3
Smruti Kshirsagar <smruti.k20@gmail.com>
2025-06-15 11:27:16 +0200

    Baseline run with cpu limits

--- demo/demo-hattrick/HATtrick/run_baseline.sh
+++ demo/demo-hattrick/HATtrick/run_baseline.sh
@@ -28,8 +28,8 @@ rm -rf "results"
 
 bash start_postgres.sh "$scale_factor"
 sleep 10
-./stats_collect_docker.sh > monitor.log 2>&1 &
-echo "Containers started. Monitoring running in background."
+#./stats_collect_docker.sh > monitor.log 2>&1 &
+#echo "Containers started. Monitoring running in background."
 
 ./HATtrickBench -init -dsn PostgresPrimary -usr myuser -pwd mypassword -pa /data/datagen -db postgres -sf "$scale_factor"
 ./HATtrickBench -frontier -dsn PostgresDocker -dsn2 PostgresReplica -usr myuser -pwd mypassword -wd "$warmup" -td "$runtime" -db postgres -t sp  -sf "$scale_factor"
